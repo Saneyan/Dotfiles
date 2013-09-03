5b78d8dbcef2e9990d815c8262e0c259fefc12bf
Saneyuki Tadokoro <saneyan@mail.gfunction.com>
2013-09-03 23:02:57 +0900

    Changed

--- .xmonad/xmobarrc.hs
+++ .xmonad/xmobarrc.hs
@@ -1,17 +1,16 @@
-Config {
-	 font = "xft:IPAPGothic:bold:size=9:antialias=true"
-       , bgColor = "black"
-       , fgColor = "grey"
-       , position = TopW L 90
-       , commands = [
-		      Run Network "eth0" ["-L", "0", "-H", "32", "--normal", "green", "--high", "red"] 10
-       	 	    , Run Cpu ["-L", "3", "-H", "30", "--normal", "green", "--high", "red"] 10
-       	 	    , Run Memory ["-t", "Mem: <usedratio>%"] 10
-		    , Run Swap [] 10
-		    , Run Date "%Y/%m/%d %a %k:%M" "date" 10
-		    , Run StdinReader
-		    ]
-       , sepChar = "%"
-       , alignSep = "}{"
-       , template = "%StdinReader% : <fc=lightblue>saneyan</fc> }{ %cpu%   %memory%   %swap%   %eth0%   <fc=#a0bc61>%date%</fc>"
-       }
+Config { font = "xft:IPAPGothic:bold:size=9:antialias=true"
+, bgColor = "black"
+, fgColor = "grey"
+, position = TopW L 90
+, commands = [
+    Run Network "eth0" ["-L", "0", "-H", "32", "--normal", "green", "--high", "red"] 10
+  , Run Cpu ["-L", "3", "-H", "30", "--normal", "green", "--high", "red"] 10
+  , Run Memory ["-t", "Mem: <usedratio>%"] 10
+  , Run Swap [] 10
+  , Run Date "%Y/%m/%d %a %k:%M" "date" 10
+  , Run StdinReader
+  ]
+, sepChar = "%"
+, alignSep = "}{"
+, template = "%StdinReader% : <fc=lightblue>saneyan</fc> }{ %cpu%   %memory%   %swap%   %eth0%   <fc=#a0bc61>%date%</fc>"
+}
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,24 +1,149 @@
+--
+-- xmonad.hs - Xmonad configuration file
+--
+-- Author: Saneyuki Tadokoro <saneyan@mail.gfunction.com>
+--
+-- Key bindings (meta key is commnad or win):
+-- Meta+Enter         switch focused window to master window
+-- Meta+Shift+C       close focused window
+-- Meta+Shift+Q       close xmonad
+-- Meta+Shift+N       switch next workspace
+-- Meta+Shift+P       switch previous workspace
+-- Meta+Shift+Enter   start a terminal (XTerm)
+-- Meta+B             start a web browser (Firefox Nightly)
+-- Meta+M             start a mailer (Thunderbird)
+-- Meta+K             start a password manager (KeePassX)
+-- Meta+C             start a cashier (GnuCash)
+-- Meta+S             start a client software of online strage (Wuala)
+-- Meta+G             start a paint software (GIMP)
+-- Meta+A             paste a private email address
+--
+-- Startup programs and workspaces:
+-- /--------------------------------------------------\
+-- | Num  | ID        | Program                       |
+-- |------+-----------+-------------------------------|
+-- | 1    | > Term    | *XTerm                        |
+-- | 2    | > Browser | *Firefox Nightly, Chromium    |
+-- | 3    | > Mailer  | *Thunderbird                  |
+-- | 4    | > Psmgr   | *KeePassX                     |
+-- | 5    | > Cashier | *GnuCash                      |
+-- | 6    | > Paint   | GIMP, Inkscape                |
+-- | 7    | > Player  | VLC                           |
+-- | 8~15 | > 8~F     | (None)                        |
+-- \--------------------------------------------------/
+-- *) These programs start after staring up xmonad
+--
+
 import XMonad
+import XMonad.Prompt.Window
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Paste(pasteString)
+import XMonad.Actions.SpawnOn(spawnOn, manageSpawn)
+import XMonad.Actions.CycleWS(nextWS, prevWS)
 
--- Start
+-- Main --
+main :: IO ()
 main = do
   xmproc <- spawnPipe "xmobar ~/.xmonad/xmobarrc.hs"
   xmonad $ defaultConfig {
-    modMask = mod4Mask
-    , layoutHook = avoidStruts $ layoutHook defaultConfig
-    , manageHook = manageDocks <+> manageHook defaultConfig
-    , logHook = dynamicLogWithPP xmobarPP {
-        ppOutput = hPutStrLn xmproc
-      , ppTitle = xmobarColor "#a0bc61" "" . shorten 50
-      }
-    , borderWidth = 2
-    , normalBorderColor = "#52799e"
-    , focusedBorderColor = "#a0bc61"
-    }
+    -- modMask is "command" key
+    modMask = myModMask
+  , workspaces = myWorkspaces
+  , layoutHook = myLayoutHook
+  , manageHook = myManageHook
+  , logHook = myLogHook xmproc
+  , startupHook = myStartupHook
+  , borderWidth = 2
+  , normalBorderColor = "#52799e"
+  , focusedBorderColor = "#a0bc61"
+  , focusFollowsMouse = False
+  }
     `additionalKeys`
-    [ ((mod4Mask .|. shiftMask, xK_Return), spawn "xterm -fd IPAPGothic -fw IPAPGothic -e tmux")
-    ]
+    myAdditionalKeys
+
+-- Default application --
+-- Double size font is IPAPGothic and starting with tmux
+myTerm            = "xterm -fd IPAPGothic -fw IPAPGothic -e tmux"
+myWebBrowser      = "firefox-nightly"
+myPasswordManager = "keepassx"
+myMailer          = "thunderbird"
+-- Use wuala client software in user directory
+myOnlineStrage    = "~/wuala/wuala"
+myCashier         = "gnucash"
+myPaint           = "gimp"
+
+{-
+ - Workspaces
+ -}
+
+-- Workspaces --
+myWorkspaces :: [WorkspaceId]
+-- It should be like "ABC > DEF > GHI > ..."
+myWorkspaces = ["> Term", "> Browser", "> Mailer", "> Psmgr", "> Cashier", "> Paint", "> Player", "> 8", "> 9", "> A", "> B", "> C", "> D", "> E", "> F"]
+
+{-
+ - Hooks 
+ -}
+
+-- Layout hook --
+myLayoutHook = avoidStruts $ layoutHook defaultConfig
+
+-- Manage hook --
+myManageHook = composeAll
+  [ className =? "XTerm"          --> doShift (myWorkspaces!!0)
+  , className =? "Firefox"        --> doShift (myWorkspaces!!1)
+  , className =? "Chromium"       --> doShift (myWorkspaces!!1)
+  , className =? "Thunderbird"    --> doShift (myWorkspaces!!2)
+  , className =? "Keepassx"       --> doShift (myWorkspaces!!3)
+  , className =? "Gnucash"        --> doShift (myWorkspaces!!4)
+  , className =? "Gimp"           --> doShift (myWorkspaces!!5)
+  , className =? "Inkscape"       --> doShift (myWorkspaces!!5)
+  , className =? "Vlc"            --> doShift (myWorkspaces!!6)
+  , manageSpawn
+  , manageDocks
+  , manageHook defaultConfig
+  ]
+
+-- Log hook --
+myLogHook h = dynamicLogWithPP xmobarPP {
+  ppOutput = hPutStrLn h
+, ppTitle = xmobarColor "#a0bc61" "" . shorten 50
+}
+
+-- Startup hook --
+myStartupHook :: X ()
+myStartupHook = do
+  spawnOn (myWorkspaces!!0) myTerm
+  spawnOn (myWorkspaces!!1) myWebBrowser
+  spawnOn (myWorkspaces!!2) myMailer
+  spawnOn (myWorkspaces!!3) myPasswordManager
+  spawnOn (myWorkspaces!!4) myCashier
+
+{-
+ - Key bindings
+ -}
+
+-- Prefix key --
+myModMask = mod4Mask
+myMSMask  = myModMask .|. shiftMask
+
+-- Additional keys --
+myAdditionalKeys =  [
+  {-
+    b: browser, k: keypass, m: mailer, s: storage
+    * Override starting term key binding
+  -}
+    ((myMSMask, xK_Return), spawn myTerm)
+  , ((myModMask, xK_b), spawn myWebBrowser)
+  , ((myModMask, xK_k), spawn myPasswordManager)
+  , ((myModMask, xK_m), spawn myMailer)
+  , ((myModMask, xK_s), spawn myOnlineStrage)
+  , ((myModMask, xK_c), spawn myCashier)
+  , ((myModMask, xK_g), spawn myPaint)
+  , ((myModMask, xK_a), pasteString "<Private email>")
+  , ((myMSMask, xK_n), nextWS)
+  , ((myMSMask, xK_p), prevWS)
+  ]
