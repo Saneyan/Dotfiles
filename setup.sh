e0b1c69a30e9bf901a8689429cdd1b9ae77c004b
TADOKORO Saneyuki <saneyan@mail.gfunction.com>
2014-11-18 04:41:10 +0900

    fix system

--- setup.sh
+++ setup.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
 
 #
+# Check system
+#
+for cmd in git zsh; do
+  if ! which $cmd &>/dev/null; then
+    echo "Before seting up dmgr, you must install ${cmd}."
+    exit 1
+  fi
+done
+
+#
 # Common setup
 #
-dirname=$(cd $(dirname $0) && pwd)
+export DMGR_SETUP_DIRNAME=$(cd $(dirname $0) && pwd)
+confdir=$HOME/.dmgr/conf
 
-ln -sv $dirname/bin $HOME/bin
-ln -sv $dirname/core $HOME/.dmgr
+ln -sv $DMGR_SETUP_DIRNAME/bin $HOME/bin
+ln -sv $DMGR_SETUP_DIRNAME/core $HOME/.dmgr
 export PATH=$PATH:$HOME/bin
 
-echo "DMGR_REPODIR=${dirname}" > $HOME/.dmgr/conf/paths/_repo
-touch $HOME/.dmgr/conf/inuse.list
-touch $HOME/.dmgr/conf/plugin.list
+echo "DMGR_REPODIR=${DMGR_SETUP_DIRNAME}" > $confdir/paths/_repo
+touch $confdir/{plugin,inuse}.list
+echo "Created $(echo ${confdir}/{plugin,inuse}.list)"
 
 dmgr enable system
+
+unset confdir DMGR_SETUP_DIRNAME
